81702b8c189e79e43abd2a8bdba71d627c046ca8
volo <massol@gmail.com>
2022-08-30 09:50:09 +0200

    v1.9

--- tools/laby_vanilla.js
+++ tools/laby_vanilla.js
@@ -1353,8 +1353,42 @@ dateAnnee.innerHTML = "Wow my game is stil playing in " +laDate.getFullYear() +"
 
 //------------------------------EGGS END---------------------------------------------------------------//
 
-/*var myWindow = window.open('Readme.md','Readme','height=800,width=600');
-*/
+//------------------------------Vue Start---------------------------------------------------------------//
+
+var app1 = new Vue({
+
+	el: '#readme',
+
+
+})
+
+
+
+
+
+
+
+
+
+
+//------------------------------Vue END---------------------------------------------------------------//
+
+
+
+
+var myWindow = window.open('Readme.md','Readme','height=800,width=600');
+
+/*var file_to_read = document.getElementById("test_json").files[0];
+*/var file_to_read = document.getElementById("test_json").files[0];
+var fileread = new FileReader();
+fileread.readAsText(file_to_read);
+fileread.onload = function (e) {
+
+	var content = e.target.result;
+	console.log(content)
+}
+
+
 /*var blob = new Blob;*/
 /*var text = new Blob(Blob.prototype.text('Readme.md'));
 */
